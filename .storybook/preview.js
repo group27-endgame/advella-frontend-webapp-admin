9c76cf3de3c4a3d0d18c42cd7e44f8429b917e58
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-05 22:34:34 +0200

    Storybook using styling

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from '../src/stylesheet'
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
+  decorators: theme,
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
