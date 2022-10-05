894c4f076d9633b588bde260748f68931f749fbd
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-05 19:39:26 +0200

    design: prepare new theme

---
+++ src/constants.tsx
@@ -0,0 +1,7 @@
+export const primaryColor = "";
+export const secondaryColor = "";
+export const tertiaryColor = "";
+export const backgroundColor = "";
+export const fontColor = "";
+export const errorColor = "";
+export const disabledColor = "";
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme, CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { backgroundColor, errorColor, primaryColor, secondaryColor } from "./constants";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: backgroundColor,
+      // paper: greyLight,
+    },
+    // action: {
+    //   disabledBackground: greenLight,
+    //   disabled: greyLight,
+    // },
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: secondaryColor,
+    },
+    error: {
+      main: errorColor,
+    },
+    // text: {
+    //   primary: white,
+    //   secondary: greyLight,
+    // },
+  },
+  // components: {
+  //   MuiAppBar: {
+  //     styleOverrides: {
+  //       colorPrimary: {
+  //         backgroundColor: greyLight,
+  //       },
+  //     },
+  //   },
+  // },
+});
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
