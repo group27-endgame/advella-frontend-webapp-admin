5de52263039c84ee494f6c877e09784b8eb0a504
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-05 23:31:21 +0200

    Revert "Update preview.js"
    This reverts commit 5a8bb8b7886984aa8259e038b75d756d0245827a.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,16 +1,4 @@
-import { ThemeProvider } from '@mui/material/styles';
-import { theme } from '../src/stylesheet'
-
-export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={theme}>
-      <Story />
-    </ThemeProvider>
-  ),
-];
-
 export const parameters = {
-  decorators: theme,
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
