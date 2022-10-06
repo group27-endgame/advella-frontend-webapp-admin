18f268a796ee35b57603763bffb9598e379aa3cd
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-06 17:53:20 +0200

    Update Login.page.tsx

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,8 +1,54 @@
 import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
 import { Container } from "@mui/system";
+import { useState } from "react";
 import LoadingButton from "../components/LoadingButton.component";
 
 function LoginPage() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [usernameErr, setUsernameErr] = useState(false);
+    const [passwordErr, setPasswordErr] = useState(false);
+
+    const [usernameErrMsg, setUsernameErrMsg] = useState("");
+    const [passwordErrMsg, setPasswordErrMsg] = useState("");
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const validateForm = () => {
+
+        let returnVal = true;
+
+        setUsernameErr(false);
+        setUsernameErrMsg("");
+        setPasswordErr(false);
+        setPasswordErrMsg("");
+
+        if(password.length < 6){
+            setPasswordErr(true);
+            setPasswordErrMsg("Password must have at least 6 characters.")
+            returnVal = false;
+        }
+        if(username.length < 1){
+            setUsernameErr(true);
+            setUsernameErrMsg("Username field cannot be empty.");
+            returnVal = false;
+        }
+
+        return returnVal;
+    }
+
+    const handleClick = () => {
+        setIsLoading(true);
+
+        if(validateForm()){
+            // call API
+        }
+
+        setIsLoading(false);
+    }
+
     return ( 
         <Container maxWidth="sm" sx={{height: "100vh", pt: 20}}>
             <Paper sx={{ p: 8 }}>
@@ -13,14 +59,14 @@ function LoginPage() {
                         </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField fullWidth={true} placeholder="Username" />
+                        <TextField fullWidth={true} error={usernameErr} helperText={usernameErrMsg} placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField fullWidth={true} placeholder="Password" type="password" />
+                        <TextField fullWidth={true} error={passwordErr} helperText={passwordErrMsg} placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </Grid>
                     <Grid item xs={12} textAlign="center">
-                        <Button variant="contained">Login</Button>
-                        <LoadingButton>Loading</LoadingButton>
+                        {!isLoading && <Button variant="contained" onClick={handleClick}>Login</Button>}
+                        {isLoading && <LoadingButton>Loading</LoadingButton>}
                     </Grid>
                 </Grid>
             </Paper>
