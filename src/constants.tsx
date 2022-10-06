15dc6bbee4210beebcbc9fc93adf279011871c40
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-06 11:59:56 +0200

    test chromatic

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-export const primaryColor = "#6A5FF3"; //#00818A
+export const primaryColor = "#00818A"; //#00818A
 export const secondaryColor = "#4a43aa";
 export const tertiaryColor = "#978ff7";
 export const backgroundColor = "#283149";
