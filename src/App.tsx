b03497f7d17cf647d1a45387d1a8b22559f47113
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-06 19:13:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import FourOhFourPage from "./pages/FourOhFour.page";
 import LoginPage from "./pages/Login.page";
-import MainPage from "./pages/Main.page";
 
 function App() {
   return (
