ef9a34b624e760fb850282e47cdbeb496298e6f3
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-06 10:10:07 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Grid, Paper, TextField, Typography } from '@mui/material';
 
 function App() {
   return (
-   <Grid container>
-    <Grid item>
-      <Paper>
-        <Typography variant='h1'>
-          Text
-        </Typography>
-        <TextField variant="outlined" />
-      </Paper>
-    </Grid>
-   </Grid>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Admin Advella WebApp - Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
