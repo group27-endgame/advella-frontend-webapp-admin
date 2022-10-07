2883b69d5fb5922877673cac0fb6f691263cb9d5
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-07 16:05:16 +0200

    404 page testing

--- src/_stories/Advella/pages/FourOhFour.stories.tsx
+++ src/_stories/Advella/pages/FourOhFour.stories.tsx
@@ -1,9 +1,10 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { BrowserRouter } from 'react-router-dom';
 import FourOhFourPage from '../../../pages/FourOhFour.page';
+import { userEvent, within } from '@storybook/testing-library';
 
 export default {
-  title: 'Advella/Pages',
+  title: 'Advella/Pages/404',
   component: FourOhFourPage,
   parameters: {
     chromatic: {
@@ -14,3 +15,11 @@ export default {
 } as ComponentMeta<typeof FourOhFourPage>;
 
 export const Page404: ComponentStory<typeof FourOhFourPage> = () => <BrowserRouter><FourOhFourPage /></BrowserRouter>;
+
+export const LinkRedirect = Page404.bind({});
+
+LinkRedirect.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const submitButton = canvas.getByRole("link");
+  await userEvent.click(submitButton);
+};
