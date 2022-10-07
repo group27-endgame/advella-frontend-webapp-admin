9f6451a2e112676ff822ef29b8e890887ad709e3
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-07 15:53:26 +0200

    Login testing

--- src/_stories/Advella/pages/LoginPage.stories.tsx
+++ src/_stories/Advella/pages/LoginPage.stories.tsx
@@ -1,11 +1,49 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import LoginPage from '../../../pages/Login.page';
+import { userEvent, within } from '@storybook/testing-library';
 
 export default {
-  title: 'Advella/Pages',
+  title: 'Advella/Pages/Login',
   component: LoginPage,
   args: {
   },
 } as ComponentMeta<typeof LoginPage>;
 
 export const PageLogin: ComponentStory<typeof LoginPage> = () => <LoginPage />;
+
+export const GoodCredentials = PageLogin.bind({});
+export const WrongCredentials = PageLogin.bind({});
+
+GoodCredentials.play = async ({ canvasElement }) => {
+  // Starts querying the component from its root
+  const canvas = within(canvasElement);
+
+  // Looks up the inputs and fills them.
+  const emailInput = canvas.getByPlaceholderText("Username");
+
+  await userEvent.type(emailInput, "Example");
+
+  const passwordInput = canvas.getByPlaceholderText("Password");
+  await userEvent.type(passwordInput, "Example");
+
+  // Looks up the button and interacts with it.
+  const submitButton = canvas.getByRole("button");
+  await userEvent.click(submitButton);
+};
+
+WrongCredentials.play = async ({ canvasElement }) => {
+  // Starts querying the component from its root
+  const canvas = within(canvasElement);
+
+  // Looks up the inputs and fills them.
+  const emailInput = canvas.getByPlaceholderText("Username");
+
+  await userEvent.type(emailInput, "");
+
+  const passwordInput = canvas.getByPlaceholderText("Password");
+  await userEvent.type(passwordInput, "");
+
+  // Looks up the button and interacts with it.
+  const submitButton = canvas.getByRole("button");
+  await userEvent.click(submitButton);
+};
