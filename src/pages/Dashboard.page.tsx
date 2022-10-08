6084c945120d28e739ce11617bb95f78af7f646a
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-08 22:08:19 +0200

    Update Dashboard.page.tsx

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -49,12 +49,6 @@ function DashboardPage() {
             <Grid item xs={12} lg={6}>
                 <PieChart graphLabel="Product/Services" labels={["Products", "Services"]} data={[350, 400]} />
             </Grid>
-            <Grid item xs={12} lg={6}>
-                <PieChart graphLabel="Product/Services" labels={["Products", "Services"]} data={[350, 400]} />
-            </Grid>
-            <Grid item xs={12} lg={6}>
-                <PieChart graphLabel="Product/Services" labels={["Products", "Services"]} data={[350, 400]} />
-            </Grid>
         </Grid>
      );
 }
