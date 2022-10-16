ecd0c4b87cdb1a92b7a6edac1b4b4f3b424d605a
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-16 16:37:36 +0200

    Update ProductsCategory.page.tsx

--- src/pages/categories/ProductsCategory.page.tsx
+++ src/pages/categories/ProductsCategory.page.tsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 function ProductsCategoryPage() {
     return ( 
         <Box>
-            Service Category
+            Product Category
         </Box>
      );
 }
