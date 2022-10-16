54690451cdefe386fd1d547392c4381a2fcf128f
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-16 16:31:54 +0200

    Services category created

---
+++ src/pages/categories/ServicesCategory.page.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function ServiceCategoryPage() {
+    return ( 
+        <Box>
+            Service Category
+        </Box>
+     );
+}
+
+export default ServiceCategoryPage;
