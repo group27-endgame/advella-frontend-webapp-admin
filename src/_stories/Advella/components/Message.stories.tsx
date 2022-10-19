cfe71d2e46cba7888bbdd6ce9bf942601dad564b
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-10-19 18:03:54 +0200

    Create Message.stories.tsx

---
+++ src/_stories/Advella/components/Message.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import MessageComponent from '../../../components/Message.component';
+
+export default {
+  title: 'Advella/Components',
+  component: MessageComponent,
+  args: {
+    messageId: 1,
+    user: {userId: 1, username: "Seymore"},
+    date: Date.now(),
+    message: "Lorem ipsum blah blah"
+  },
+} as ComponentMeta<typeof MessageComponent>;
+
+export const MessageCard: ComponentStory<typeof MessageComponent> = (props) => <MessageComponent {...props} />;
