979bcf6e978de8b269c98446c57e1b4af2f9b477
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-11-01 09:10:38 +0100

    Update ServicesDashboard.page.tsx

--- src/pages/dashboard/ServicesDashboard.page.tsx
+++ src/pages/dashboard/ServicesDashboard.page.tsx
@@ -1,45 +1,12 @@
 import { Grid } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
 import ActionListComponent, { IActionList } from "../../components/ActionList.component";
 import BarChartComponent from "../../components/BarChart.component";
 import MapChartComponent from "../../components/MapChart.component";
-
-const list: IActionList[] = [
-  {
-    listId: 1,
-    title: "Car Cleaning",
-    dateTime: Date.now()-500000,
-    type: "service",
-    subscription: "Added by Seymore",
-  },
-  {
-    listId: 2,
-    title: "Bike Cleaning",
-    dateTime: Date.now()-1500000,
-    type: "service",
-    subscription: "Added by Seymore",
-  },
-  {
-    listId: 3,
-    title: "Home Cleaning",
-    dateTime: Date.now()-2500000,
-    type: "service",
-    subscription: "Added by Seymore",
-  },
-  {
-    listId: 4,
-    title: "Windows Cleaning",
-    dateTime: Date.now()-3500000,
-    type: "service",
-    subscription: "Added by Seymore",
-  },
-  {
-    listId: 5,
-    title: "General Cleaning",
-    dateTime: Date.now()-4500000,
-    type: "service",
-    subscription: "Added by Seymore",
-  },
-];
+import { getMonthList } from "../../constants";
+import ServiceService from "../../services/Service.service";
+import { LottieLoading } from "../../_stories/Advella/components/Loading.stories";
 
 const regions = [
   { city: "North Jutland", value: 1000 },
@@ -59,6 +26,63 @@ const regions = [
 ];
 
 function ServicesDashboardPage() {
+
+  const [cookie, ,] = useCookies(["token"]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [monthList, setMonthList] = useState<string[]>([]);
+  const [monthListServices, setMonthListServices] = useState<number[]>([]);
+  const [latestServices, setLatestServices] = useState<IActionList[]>([]);
+
+  useEffect(() => {
+      const serviceService: ServiceService = new ServiceService();
+
+      setMonthList(getMonthList(new Date().getMonth()));
+
+      const monthProds: {month: string, value: number}[] = [];
+      const latestActions: IActionList[] = [];
+
+      Promise.all(
+          [
+              getMonthList(new Date().getMonth()).map((d) => {
+                  serviceService
+                    .getTotalCount(
+                      cookie.token,
+                      Date.parse(`1 ${d}`),
+                      Date.parse(`31 ${d}`)
+                    )
+                    .then((res) => {
+                      monthProds.push({month: d, value: res});
+                    });
+                }),
+                serviceService.getLatestServices(cookie.token, 5).then(res => {
+                  res.map(p => {
+                      latestActions.push({
+                          listId: p.serviceId,
+                          title: p.title,
+                          dateTime: p.postedDateTime,
+                          subscription: "",
+                          type: "service"
+                      })
+                  })
+                }).catch(err => console.log(err))
+          ]
+      ).then(() => {
+          setIsLoading(false);
+    
+          monthProds.sort((s1, s2) => {
+              return new Date(`1 ${s1.month}`).getTime() - new Date(`1 ${s2.month}`).getTime();
+          });
+          const sp: number[] = [];
+          monthProds.map(m => sp.push(m.value));
+          setMonthListServices(sp);
+          setLatestServices(latestActions);
+      })
+  }, []);
+
+  if(isLoading)
+      return <LottieLoading open={isLoading} />
+      
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -67,12 +91,12 @@ function ServicesDashboardPage() {
       <Grid item md={6}>
         <BarChartComponent
           graphLabel="Services per Month"
-          labels={["January", "February", "March", "April"]}
-          data={[350, 450, 590, 780]}
+          labels={monthList}
+          data={monthListServices}
         />
       </Grid>
       <Grid item md={6}>
-        <ActionListComponent title="newest Services" list={list} />
+        <ActionListComponent title="newest Services" list={latestServices} />
       </Grid>
     </Grid>
   );
