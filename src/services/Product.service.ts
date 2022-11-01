d0ef47828a41344bb0aba7ccf59004a61848dcb0
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-11-01 07:22:12 +0100

    Endpoints changed

--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -4,13 +4,13 @@ import { ProductModel } from "../models/Product.model";
 
 export default class ProductService {
     public async getTotalCount(token: string): Promise<number> {
-        const response = await axios.get(`${backendUrl}/api/products/count`, {headers: { Authorization: `Bearer ${token}` } });
+        const response = await axios.get(`${backendUrl}/api/products/dash-board/count`, {headers: { Authorization: `Bearer ${token}` } });
 
         return response.data
     }
 
     public async getLatestProducts(token: string, amount: number = 5): Promise<ProductModel[]>{
-        const response = await axios.get(`${backendUrl}/api/products/latest?amount=${amount}`, {headers: { Authorization: `Bearer ${token}` } });
+        const response = await axios.get(`${backendUrl}/api/products/dash-board/latest?amount=${amount}`, {headers: { Authorization: `Bearer ${token}` } });
 
         return response.data as ProductModel[];
     }
