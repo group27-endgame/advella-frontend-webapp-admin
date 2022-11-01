9c21eb45abfa940da898e5fe34d1ccbae4bb8a82
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-11-01 11:51:49 +0100

    Create ServiceCategory.service.ts

---
+++ src/services/ServiceCategory.service.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { backendUrl } from "../links";
+import { ServiceCategoryModel } from "../models/ServiceCategory.model";
+
+export default class ServiceCategoryService{
+    public async getAllCategories(token: string): Promise<ServiceCategoryModel[]>{
+        const response = await axios.get(`${backendUrl}/api/service-categories/dash-board/all`, {headers: { Authorization: `Bearer ${token}` } });
+
+        return response.data as ServiceCategoryModel[];
+    }
+
+    public async deleteCategory(categoryId: number, token: string): Promise<void>{
+        const response = await axios.delete(`${backendUrl}/api/service-categories/dash-board/${categoryId}`, {headers: { Authorization: `Bearer ${token}` } });
+    }
+
+    public async editCategory(category: ServiceCategoryModel, token: string): Promise<void>{
+        const response = await axios.put(`${backendUrl}/api/service-categories/dash-board`, category, {headers: { Authorization: `Bearer ${token}` } });
+    }
+
+    public async addCategory(category: ServiceCategoryModel, token: string): Promise<void>{
+        const response = await axios.post(`${backendUrl}/api/service-categories/dash-board`, category, {headers: { Authorization: `Bearer ${token}` } });
+    }
+}
