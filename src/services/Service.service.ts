b92d1c41d9451d55f13d6e0460e7ca2d6ece2eea
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-11-01 09:10:35 +0100

    Updating Service

--- src/services/Service.service.ts
+++ src/services/Service.service.ts
@@ -3,8 +3,8 @@ import { backendUrl } from "../links";
 import { ServiceModel } from "../models/Service.model";
 
 export default class ServiceService {
-    public async getTotalCount(token: string): Promise<number> {
-        const response = await axios.get(`${backendUrl}/api/services/dash-board/count`, {headers: { Authorization: `Bearer ${token}` } });
+    public async getTotalCount(token: string, startDate: number = 0, endDate: number = Date.now()): Promise<number> {
+        const response = await axios.get(`${backendUrl}/api/services/dash-board/${startDate}/${endDate}`, {headers: { Authorization: `Bearer ${token}` } });
 
         return response.data
     }
